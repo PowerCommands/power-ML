0364c4c9567ac82d4ead62f1c526d5d9744fc2d5
Harri Klingsten <powercommand2022@gmail.com>
2023-01-24 20:58:48 +0100

    refactoring

--- src/PowerML/PowerMLCommands/Commands/Docker.cs
+++ src/PowerML/PowerMLCommands/Commands/Docker.cs
@@ -2,6 +2,7 @@
 namespace PowerMLCommands.Commands;
 
 [PowerCommandDesign( description: "Run docker commands",
+              overrideHelpOption: true,
                          example: "docker run -p 9000:9000 -p 9001:9001 minio/minio server /data --console-address \":9001\"")]
 public class DockerCommand : MasterCommando
 {
--- src/PowerML/PowerMLCommands/Commands/KCommand.cs
+++ src/PowerML/PowerMLCommands/Commands/KCommand.cs
@@ -2,9 +2,9 @@
 
 namespace PowerMLCommands.Commands;
 
-[PowerCommandTest(         tests: " ")]
 [PowerCommandDesign( description: "Run kubectl commands using the alias k",
-    example: "k")]
+              overrideHelpOption: true,
+                         example: "k")]
 public class KCommand : MasterCommando
 {
     public KCommand(string identifier, PowerCommandsConfiguration configuration) : base(identifier, configuration, alias: "kubectl") { }
